2e5afe6cfc948cc7777d1b17d9c107c6bbc222fc
Jhonatan Sumback <jholsumback3@gmail.com>
2025-02-17 19:10:12 -0300

    Update Primeiroprograma.py

--- Primeiroprograma.py
+++ Primeiroprograma.py
@@ -1,33 +1,3 @@
-#sistema bancário
-#depósito / Saque e Extrato
-#DEPÓSITOS: valores positivos, apenas 1 usuário, deposito em uma variável e na opção de extrato
-#SAQUE: 3 saques diários e limite máximo de R$500 por saque, caso usuário não tenha saldo em conta, o sistema deve exibir uma msg informando que não será possível sacar. Todos os saques devem ser armazenados
-#em uma variável e exibidos na operação extrato.
-#EXTRATO: listar todos os saques e depósitos, no fim deve ser exibido o saldo atual da conta em R$.
-
-#Método dos 5Qs:
-
-#1- Quais são os dados de entrada necessários?
-    #perguntar ao usuário qual opção ele deseja fazer, sacar, depositar ou ver extrato
-
-#2- O que devo fazer com esses dados?
-    #Se for depósito valores positivos, apenas 1 usuário, deposito em uma variável e na opção de extrato, se for SAQUE 3 saques diários e limite máximo de R$500 por saque, caso usuário não tenha saldo em conta, o sistema deve exibir uma msg informando que não será possível sacar. Todos os saques devem ser armazenados
-#em uma variável e exibidos na operação extrato e se for EXTRATO: listar todos os saques e depósitos, no fim deve ser exibido o saldo atual da conta em R$.
-
-#3- Quais as restrições do problema?
-    #deve ter essas 3 opções para a conta do cliente
-
-#4- Qual o resultado esperado?
-    #resultado esperado é so cliente conseguir realizar saque, depósito e ver extrato
-
-#5- Qual a sequencia de passos a ser feitos para chegar ao resultado esperado?
-    #input ao usuário qual opção ele deseja realizar: saque, saldo ou extrato
-    #[1]deposito >=100
-    #   extrato
-    #[2]Saque <= 500/dia 
-        #saldo < saque exibir saldo insuficiente
-    #[3]Extrato = saques, depositos
-        #print saldo atual da conta
 
 menu = """
 
